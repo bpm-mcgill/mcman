82284188ebec6c75e14745d3d00993f4505b479f
Brayden McGill <brayden@mobilebayarea.com>
2025-05-22 18:51:29 -0500

    Template Starter Code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("yart");
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{
+    DefaultTerminal, Frame
+};
+
+fn main() -> Result<()>{
+    let terminal = ratatui::init();
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)){
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("Yart", frame.area());
 }
