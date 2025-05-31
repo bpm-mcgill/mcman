b985029cc3dfe8e693a1ded41b96cfc97eac7e44
Brayden McGill <brayden@mobilebayarea.com>
2025-05-31 02:47:19 -0500

    Ratatui testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
 pub mod installer;
+
+// leaving this here for future reference
+    // let path = "/home/user/Documents/install_test/";
+    // let client = Client::new();
+    // let mut installer = ServerInstaller::new(client);
+
+    // let versions = installer.get_versions().await?;
+    // let latest_mc = versions.first().unwrap();
+    // let compat = installer.get_loader_compat(latest_mc, LoaderType::Fabric).await?.unwrap();
+    // let latest_fab = compat.first().unwrap();
+    
+    // let serv = ServerKind::Modded { mc: latest_mc.into(), loadertype: LoaderType::Fabric, version: latest_fab.into() };
+    // let serv = ServerKind::Vanilla { mc: latest_mc.into() };
+    // installer.install_server(serv, Path::new(path)).await?; 
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,132 @@
-use mcman::installer::{LoaderType, ServerInstaller, ServerKind};
-use std::path::Path;
+use std::io;
+
+use mcman::installer::ServerInstaller;
+use ratatui::{crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind}, layout::{Constraint, Direction, Flex, Layout, Rect, Spacing}, style::{Color, Style, Stylize}, symbols::border, text::{Line, Text}, widgets::{Block, BorderType, Borders, List, ListState, Paragraph, StatefulWidget, Widget}, DefaultTerminal, Frame};
 use reqwest::Client;
+
+struct App {
+    exit: bool,
+    list: ServerListWidget,
+
+    client: Client,
+    installer: ServerInstaller
+}
+
+impl App {
+    pub fn new() -> Self {
+        let client = Client::new();
+        App {
+            exit: false,
+            list: ServerListWidget::new(Vec::new()),
+            installer: ServerInstaller::new(client.clone()),
+            client
+        }
+    }
+
+    pub async fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        let versions = self.installer.get_versions().await.unwrap();
+        self.list.items = versions;
+        while !self.exit {
+            terminal.draw(|frame| self.render(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    pub fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+    
+    pub fn render(&mut self, frame: &mut Frame) {
+        let [area] = Layout::horizontal([Constraint::Percentage(65)]).flex(Flex::Center).areas(frame.area());
+        let [area] = Layout::vertical([Constraint::Percentage(65)]).flex(Flex::Center).areas(area);
+        frame.render_widget(&mut self.list, area);
+        
+        let b = Block::new().title_bottom(Line::from("mcman v0.0.12").right_aligned());
+        frame.render_widget(b,frame.area());
+    }
+
+    pub fn handle_key_event(&mut self, event: KeyEvent) {
+        match event.code {
+            KeyCode::Char('q') => {self.exit = true},
+            KeyCode::Up => {
+                self.list.select_prev();
+            }
+            KeyCode::Down => {
+                self.list.select_next();
+            }
+            _ => {}
+        }
+    }
+}
+
+struct ServerListWidget {
+    items: Vec<String>,
+    state: ListState
+}
+
+impl ServerListWidget {
+    fn new(items: Vec<String>) -> ServerListWidget {
+        ServerListWidget {
+            items,
+            state: ListState::default().with_selected(Some(0)), 
+        }
+    }
+    
+    fn select_prev(&mut self){
+        if let Some(index) = self.state.selected() {
+            let idx = index.saturating_sub(1);
+            self.state.select(Some(idx));
+        }
+    }
+
+    fn select_next(&mut self){
+        if let Some(index) = self.state.selected() {
+            let idx = index.saturating_add(1);
+            self.state.select(Some(idx));
+        }
+    }
+}
+
+impl Widget for &mut ServerListWidget {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+        where
+            Self: Sized {
+        let title = Line::from(" Server List ".bold());
+        let keybinds = Line::from(vec![
+            " New Server".into(),
+            "<N>".blue().bold(),
+            "  Delete Server".into(),
+            "<D>".blue().bold(),
+            "  Select".into(),
+            "<Enter> ".blue().bold(),
+        ]);
+        let block = Block::bordered().title(title.centered()).title_bottom(keybinds.centered()).border_set(border::ROUNDED); 
+        let list = List::new(self.items.clone())
+            .highlight_symbol("> ")
+            .highlight_style(
+                Style::new()
+                .fg(Color::Black)
+                .bg(Color::Gray).bold()
+            )
+            .block(block);
+        StatefulWidget::render(list, area, buf, &mut self.state);
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let path = "/home/user/Documents/install_test/";
-    let client = Client::new();
-    let mut installer = ServerInstaller::new(client);
+    let mut app = App::new();
 
-    let versions = installer.get_versions().await?;
-    let latest_mc = versions.first().unwrap();
-    let compat = installer.get_loader_compat(latest_mc, LoaderType::Fabric).await?.unwrap();
-    let latest_fab = compat.first().unwrap();
+    let mut terminal = ratatui::init();
+    app.run(&mut terminal).await;
     
-    // let serv = ServerKind::Modded { mc: latest_mc.into(), loadertype: LoaderType::Fabric, version: latest_fab.into() };
-    let serv = ServerKind::Vanilla { mc: latest_mc.into() };
-    installer.install_server(serv, Path::new(path)).await?; 
-    
-
-    // println!("Version types to fetch (R: release, S: snapshots, B: betas, A: alphas): ");
-    // let mut input = String::new();
-
-    // io::stdin().read_line(&mut input).expect("Error reading input fuckass");
-    // let first = input.trim().chars().next();
-    // let list_type = match first {
-        // Some('R') => "release",
-        // Some('S') => "snapshot",
-        // Some('B') => "old_beta",
-        // Some('A') => "old_alpha",
-        // _ => "release"
-    // };
-    
-    // let version_list = handler.get_versions(list_type).await?;
-    // println!("{version_list:?}");
-
-    // println!("Select a version: ");
-    // input.clear();
-    // io::stdin().read_line(&mut input).expect("Error reading input fuckass");
-    // let trimd = input.trim();
-    
-    // just throw an error if wrong version for testing
-    // version will also hold the same thing as trimd but who care lowk
-    // let version = version_list.iter().find(|v| *v == trimd).expect("Version not found");
-
-    // 80% sure all versions after 1.2.5 have server .jars
-    //handler.download_version(trimd).await?;
-    
-    // println!("Pick a fabric loader version:");
-    // input.clear();
-    // io::stdin().read_line(&mut input).expect("Error reading input fuckass");
-    // let trimd = input.trim();
-
+    ratatui::restore();
     Ok(())
 }
